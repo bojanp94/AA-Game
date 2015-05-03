de19a83c649c866927532601f7fe14ccd3edd9b3
bojanp94 <petrovskib94@gmail.com>
2015-05-03 02:40:46 +0200

    initial commit

---
+++ AA/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AA
+{
+    public partial class Form1 : Form
+    {
+        Rotator Rotator;
+        public Form1()
+        {
+            InitializeComponent();
+            Rotator = new Rotator(0.1);
+            //Rotator.Balls.Add(new Ball(0));
+            //Rotator.Balls.Add(new Ball(90));
+            //Rotator.Balls.Add(new Ball(180));
+            //Rotator.Balls.Add(new Ball(270));
+
+            for (int i = 0; i < 20; i++)
+            {
+                Rotator.Balls.Add(new Ball(i*(360/20)));
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Rotator.Draw(e.Graphics);
+        }
+    }
+}
---
+++ AA/Rotator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA
+{
+    public class Rotator
+    {
+        public double Angle { get; set; }
+        public double Speed { get; set; }
+        public int Radius { get; set; }
+        public  const int BallAngle = 12;
+        public const double DegToRad = Math.PI / 180.0;
+        public  const int StickRatio = 3;
+        public  const int Padding = 200;
+        public List<Ball> Balls;
+        Brush Brush; 
+
+
+        public Rotator (double speed)
+        {
+            Angle = 0.0;
+            Speed = speed;
+            Radius = 50;
+            Balls = new List<Ball>();           
+            Brush = new SolidBrush (Color.Black);
+            Ball.Radius = (int)(Radius * StickRatio * Math.Tan(BallAngle * DegToRad));
+            
+
+        }
+
+        public void Draw (Graphics g)
+        {
+            g.FillEllipse(Brush, Padding, Padding, Radius * 2, Radius * 2);
+            foreach (var ball in Balls)
+            {
+                var current_angle = ball.Angle;
+                current_angle += Angle;
+                current_angle %= 360.0;
+                var coords = GetCoordinatesForBall(new Point(Padding + Radius, Padding + Radius), current_angle);
+                g.FillEllipse(Brush, coords.X, coords.Y, Ball.Radius, Ball.Radius);
+
+            }
+
+        }
+        public Point GetCoordinatesForBall(Point start, double angle)
+        {
+            var X = (int)(start.X + Math.Cos(angle * DegToRad) * Radius * StickRatio - Ball.Radius/2);
+            var Y = (int)(start.X + Math.Sin(angle * DegToRad) * Radius * StickRatio - Ball.Radius/2);
+            return new Point(X,Y);
+        }
+
+        
+
+    }
+}
